f797e38a5ada06340832e5b96c352d932fbe79da
Gabriel <gabrielm.linassi@gmail.com>
2021-01-24 22:41:27 -0300

    Revert "Revert "First commit""
    This reverts commit 2a54c0d9bf3e450be4f7e1186afe85444963975b.

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,60 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Layout from "../components/Layout";
+import Link from "next/link";
 
-export default function Home() {
+export default function Home({ pokemons }) {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <Layout title="NextJS Pokedex">
+      <div className="flex items-center mb-8">
+        <img src="/logo.png" alt="Logo" className="mr-3" />
+        <h1 className="text-4xl font-extrabold">Pokedex</h1>
+      </div>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <ul>
+        {pokemons.map((pokemon, _index) => {
+          const index = _index + 1;
+          return (
+            <li key={index} id={index}>
+              <Link href={`/pokemon?id=${index}`}>
+                <a className="flex items-center text-lg bg-gray-200 hover:bg-gray-300 shadow-md my-2 p-4 rounded-md">
+                  <img
+                    src={pokemon.image}
+                    alt={pokemon.name}
+                    className="w-20 mr-3"
+                  />
+                  <div className="flex items-center  capitalize">
+                    <span className="font-bold mr-2">{index}.</span>
+                    {pokemon.name}
+                  </div>
+                </a>
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </Layout>
+  );
+}
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+export async function getStaticProps(context) {
+  try {
+    const res = await fetch("https://pokeapi.co/api/v2/pokemon/?limit=150");
+    const { results } = await res.json();
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+    const pokemons = results.map((result, _index) => {
+      const index = _index + 1;
+      const image = `https://pokeres.bastionbot.org/images/pokemon/${index}.png`;
+      return {
+        ...result,
+        image,
+      };
+    });
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+    return {
+      props: {
+        pokemons: pokemons,
+      },
+    };
+  } catch (error) {
+    console.log(error);
+  }
 }
